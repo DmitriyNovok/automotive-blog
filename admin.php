53125d52716e2ca9649feaa014a258055cb6e15b
DmitriyNovok <steamap@mail.ru>
2018-07-30 17:00:46 +0300

    fix empty dispatch

--- admin.php
+++ admin.php
@@ -4,6 +4,9 @@ session_start();
 if ($_SESSION['user_id']) {
     require_once 'loader.php';
     $dispatch = $_REQUEST['dispatch'];
+    if ($_REQUEST['dispatch'] == NULL) {
+        header('Location: admin.php?dispatch=index');
+    }
     require_once 'controllers/admin/' . $dispatch . '.php';
 } else {
     unset($_REQUEST);
--- index.php
+++ index.php
@@ -4,5 +4,8 @@ session_start();
 require_once 'loader.php';
 
 $dispatch = $_REQUEST['dispatch'];
-
-require_once 'controllers/front/' . $dispatch . '.php';
+if ($_REQUEST['dispatch'] == NULL) {
+    header('Location: index.php?dispatch=index');
+} else {
+    require_once 'controllers/front/' . $dispatch . '.php';
+}
