15759e8ad25fd073ecc9f6dc518aa72b4c654b0b
DmitriyNovok <steamap@mail.ru>
2019-07-20 23:47:00 +0300

    FIX

--- templates/front/article_preview.php
+++ templates/front/article_preview.php
@@ -4,5 +4,5 @@
     <h5>Автор: <?= $article->user->name ?></h5>
     <h5>Дата публикации: <?= $article->date ?></h5>
     <p><?= $article->text ?></p>
-    <a href="/test/testRoutes/Hello" class="btn btn-primary">Читать далее</a>
+    <a href="index.php?dispatch=article&id=<?= $article->id ?>" class="btn btn-primary">Читать далее</a>
 </div>
