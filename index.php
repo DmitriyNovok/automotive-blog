14dfe76fbf2c0eb227569f65d7289aeac600ba60
Dmitry Novokreshennyh <d.novokreshennyh@key-g.com>
2019-10-09 22:38:11 +0300

    test tg bot

--- index.php
+++ index.php
@@ -139,20 +139,25 @@ function processMessage($message) {
         // incoming text message
         $text = $message['text'];
 
-        if (strpos($text, "/start") === 0) {
-            apiRequestJson("sendMessage", array('chat_id' => $chat_id, "text" => 'Hello', 'reply_markup' => array(
-                'keyboard' => array(array('Hello', 'Hi')),
-                'one_time_keyboard' => true,
-                'resize_keyboard' => true)));
-        } else if ($text === "Hello" || $text === "Hi") {
-            apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => 'Nice to meet you'));
-        } else if (strpos($text, "/stop") === 0) {
-            apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => 'Stop ? Can not stop me , BRO! ;)'));
-        } else {
-            apiRequestWebhook("sendMessage", array('chat_id' => $chat_id, "reply_to_message_id" => $message_id, "text" => 'Cool'));
+        switch ($text) {
+            case '/start';
+                apiRequestJson("sendMessage", array('chat_id' => $chat_id, "text" => 'Hello', 'reply_markup' => array(
+                    'keyboard' => array(array('Hello', 'Hi')),
+                    'one_time_keyboard' => true,
+                    'resize_keyboard' => true)));
+                break;
+
+            case 'Hello';
+                apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => 'Nice to meet you'));
+                break;
+
+            case '/stop';
+                apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => 'Stop ? Can not stop me , BRO! ;)'));
+                break;
+
+            default:
+                apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => 'I understand only text messages'));
         }
-    } else {
-        apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => 'I understand only text messages'));
     }
 }
 
